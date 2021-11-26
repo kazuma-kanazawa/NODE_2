fe260f0a193bb6c48cdd2db87cbf77b560760a7b
kazuma-kanazawa <200033qb@yse-c.net>
2021-11-26 09:59:23 +0900

    200033

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ console.log(chalk.green('Hello YSE!!!'))
 console.log(chalk.blue('Hello YSE!!!'))
 
 
-const cities = require('cities')
+//const cities = require('cities')
+const lat = process.aray[3]
+const lng = process.aray[2]
+
 if(lat && lng) {
     let city = cities.gps_lookup(lat,lng)
     console.log(city)
